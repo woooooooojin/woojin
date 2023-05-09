411727bb709a18495c13b2a8dea44067eca941e4
woojin <dnwls5995@naver.com>
2023-05-09 10:51:38 +0900

    0509

---
+++ javascript/js/data.js
@@ -0,0 +1,34 @@
+const shoppingList = [
+  {
+    id: "sam05",
+    name: "1953세트 2호",
+    price: 9000,
+    src: "./js/images/best_04.png"
+  },
+  {
+    id: "sam04",
+    name: "캠핑어묵탕",
+    price: 5000,
+    src: "./js/images/best_01.png"
+  },
+  {
+    id: "sam03",
+    name: "모듬어묵",
+    price: 4000,
+    src: "./js/images/best_02.png"
+  },
+  {
+    id: "sam02",
+    name: "실속모듬어묵",
+    price: 7000,
+    src: "./js/images/best_03.png"
+  },
+  {
+    id: "sam01",
+    name: "1953세트 2호",
+    price: 9000,
+    src: "./js/images/best_04.png"
+  },
+]
+
+export default shoppingList
