77613ea8de42891501355be332e55b51a062281d
woojin <dnwls5995@naver.com>
2023-05-09 15:57:40 +0900

    0509

--- fila_team/js/sub01.js
+++ fila_team/js/sub01.js
@@ -3,11 +3,100 @@ import subData from "./sub_data.js";
 
 const subList = document.querySelector('.sublist')
 
-for(let i =0; i<subData.length; i++){
+for (let i = 0; i < subData.length; i++) {
 
     const subDiv = document.createElement('div')
-    subDiv.setAttribute('class','subBox')
+    subDiv.setAttribute('class', 'subBox')
 
-    
+
+
+    const subImgWrap = document.createElement('div')
+    subDiv.appendChild(subImgWrap)
+    subImgWrap.setAttribute('class', 'subimg_wrap')
+
+
+    // hover likes and cart
+    const hoverBox = document.createElement('div')
+    subImgWrap.appendChild(hoverBox)
+    hoverBox.setAttribute('class', 'hoverBox')
+
+    const hoverWrap = document.createElement('div')
+    hoverBox.appendChild(hoverWrap)
+    hoverWrap.setAttribute('class','hoverWrap')
+
+    const like = document.createElement('div')
+    const likeTxt = document.createElement('i')
+
+    likeTxt.setAttribute('class','fas fa-heart')
+    like.setAttribute('class','like')
+    like.appendChild(likeTxt)
+    hoverWrap.appendChild(like)
+
+    const cart = document.createElement('div')
+    const cartTxt = document.createElement('i')
+    cartTxt.setAttribute('class','fas fa-shopping-cart')
+    cart.appendChild(cartTxt)
+    cart.setAttribute('class','cart')
+    hoverWrap.appendChild(cart)
+
+
+    //img
+    const subImg = document.createElement('img')
+    subImg.setAttribute('src', subData[i].src)
+    subImgWrap.appendChild(subImg)
+    subImg.setAttribute('class', 'subimg')
+
+
+
+    //상품설명
+    const itemWrap = document.createElement('div')
+    subDiv.appendChild(itemWrap)
+    itemWrap.setAttribute('class', 'item_wrap')
+
+    const itemDescWrap = document.createElement('div')
+    itemWrap.appendChild(itemDescWrap)
+    itemDescWrap.setAttribute('class', 'item_desc_wrap')
+
+
+
+    const itemCategory = document.createElement('p')
+    const itemCategoryTxt = document.createTextNode(subData[i].category)
+    itemCategory.appendChild(itemCategoryTxt)
+    itemDescWrap.appendChild(itemCategory)
+    itemCategory.setAttribute('class', 'item_category')
+
+
+    const itemName = document.createElement('p')
+    const itemNameTxt = document.createTextNode(subData[i].name)
+    itemName.appendChild(itemNameTxt)
+    itemDescWrap.appendChild(itemName)
+    itemName.setAttribute('class', 'item_name')
+
+
+    const itemPrice = document.createElement('p')
+    const itemPriceTxt = document.createTextNode(subData[i].price)
+    itemPrice.appendChild(itemPriceTxt)
+    itemDescWrap.appendChild(itemPrice)
+    itemPrice.setAttribute('class', 'item_price')
+
+
+    //구매버튼
+    const buyBox = document.createElement('div')
+    itemWrap.appendChild(buyBox)
+    buyBox.setAttribute('class', 'buy_box')
+
+    const buyBtn = document.createElement('div')
+    const buyTxt = document.createTextNode('BUY')
+    buyBtn.appendChild(buyTxt)
+    buyBox.appendChild(buyBtn)
+    buyBtn.setAttribute('class', 'buy_btn')
+
+
+
+
+
+    subList.appendChild(subDiv)
+
+
 
 }
--- fila_team/js/sub_data.js
+++ fila_team/js/sub_data.js
@@ -4,119 +4,140 @@ const subData = [
     category:"공용 컴포트핏",
     name:"컴포트핏 우븐 후드티",
     price:"119,000원",
-    src :"../img/subimg/sub01"
+    src :"../img/subimg/sub01.jpg"
 },
 {
     id:"sub02",
     category:"공용",
     name:"스몰로고 카라 티",
     price:"79,000원",
-    src : "../img/subimg/sub02"
+    src : "../img/subimg/sub02.jpg"
 },
 {
     id:"sub03",
     category:"공용 컴포트핏",
     name:"컴포트핏 플래그 반팔티",
     price:"39,000원",
-    src : "../img/subimg/sub03"
+    src : "../img/subimg/sub03.jpg"
 },
 {
     id:"sub04",
     category:"공용 컴포트핏",
     name:"컴포트핏 플래그 반팔티",
     price:"39,000원",
-    src : "../img/subimg/sub04"
+    src : "../img/subimg/sub04.jpg"
 },
 {
     id:"sub05",
     category:"공용 컴포트핏",
     name:"컴포트핏 마인드블로어 반팔티",
     price:"39,000원",
-    src : "../img/subimg/sub05"
+    src : "../img/subimg/sub05.jpg"
 },
 {
     id:"sub06",
     category:"공용",
     name:"테니스 배색 카라티셔츠",
     price:"99,000원",
-    src : "../img/subimg/sub06"
+    src : "../img/subimg/sub06.jpg"
 },
 {
     id:"sub07",
     category:"공용 오버핏",
     name:"테잎 오버핏 바람막이 자켓",
     price:"99,000원",
-    src : "../img/subimg/sub07"
+    src : "../img/subimg/sub07.jpg"
 },
 {
     id:"sub08",
     category:"남성",
     name:"퍼포먼스 경량 4.5부 숏팬츠",
     price:"69,000원",
-    src : "../img/subimg/sub08"
+    src : "../img/subimg/sub08.jpg"
 },
 {
     id:"sub09",
     category:"공용 오버핏",
     name:"테잎 오버핏 우븐 반바지",
     price:"79,000원",
-    src : "../img/subimg/sub09"
+    src : "../img/subimg/sub09.jpg"
 },
 {
     id:"sub10",
     category:"공용",
     name:"테니스 퍼포먼스 반팔티",
     price:"49,000원",
-    src : "../img/subimg/sub10"
+    src : "../img/subimg/sub10.jpg"
 },
 {
     id:"sub11",
     category:"공용",
     name:"라이프 레이어드 반팔티",
     price:"45,000원",
-    src : "../img/subimg/sub11"
+    src : "../img/subimg/sub11.jpg"
 },
 {
     id:"sub12",
     category:"남성 컴포트핏 테니스",
     name:"테니스 컴포트핏 니트 카라티",
     price:"45,000원",
-    src : "../img/subimg/sub12"
+    src : "../img/subimg/sub12.jpg"
 },
 {
     id:"sub13",
     category:"공용 컴포트핏 라이프스타일",
     name:"컴포트핏 조거팬츠",
     price:"109,000원",
-    src : "../img/subimg/sub13"
+    src : "../img/subimg/sub13.jpg"
 },
 {
     id:"sub14",
     category:"공용 오버핏 라이프스타일",
     name:"오버핏 이지팬츠",
     price:"99,000원",
-    src : "../img/subimg/sub14"
+    src : "../img/subimg/sub14.jpg"
 },
 {
     id:"sub15",
     category:"공용 오버핏 라이프스타일",
     name:"오버핏 스트링 와이드 팬츠",
     price:"129,000원",
-    src : "../img/subimg/sub15"
+    src : "../img/subimg/sub15.jpg"
 },
 {
     id:"sub16",
     category:"남성",
     name:"스포츠 쿨링 반바지",
     price:"49,000원",
-    src : "../img/subimg/sub16"
+    src : "../img/subimg/sub16.jpg"
 },
 {
     id:"sub17",
     category:"공용 레귤러핏 라이프스타일",
     name:"스트레치 카고 조거팬츠",
     price:"119,000원",
-    src : "../img/subimg/sub17"
+    src : "../img/subimg/sub17.jpg"
+},
+{
+    id:"sub18",
+    category:"공용 라이프스타일",
+    name:"클래식킥스 B V3",
+    price:"45,000원",
+    src : "../img/subimg/sub18.jpg"
+},
+{
+    id:"sub19",
+    category:"공용 라이프스타일",
+    name:"FILA athletics 아노락",
+    price:"87,200원",
+    src : "../img/subimg/sub19.jpg"
+},
+{
+    id:"sub20",
+    category:"공용 라이프스타일",
+    name:"White Line 라켓백",
+    price:"119,000원",
+    src : "../img/subimg/sub20.jpg"
 },
 
 
