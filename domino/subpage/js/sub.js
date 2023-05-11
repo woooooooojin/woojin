534e3da131b2be5f525ccc601902b1ce886ff56c
woooooooojin <kevin2205@naver.com>
2023-05-11 22:08:50 +0900

    0511

--- domino/subpage/js/sub.js
+++ domino/subpage/js/sub.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const numBox = document.querySelector('#hp5')
-   
-    
+
+
     const randomNum = Math.floor(Math.random() * 10001)
     let randomTxt = document.createTextNode(randomNum)
 
@@ -11,22 +11,43 @@ document.addEventListener('DOMContentLoaded', function () {
     hpBtn.addEventListener('click', function () {
 
         numBox.appendChild(randomTxt);
-        
+
     })
 
-    
+
     const hp4Btn = document.getElementById('hp4_btn');
     const hp4Value = document.getElementById('hp4')
-    hp4Btn.addEventListener('click',function(){
+    const numPop = document.querySelector('.num_chk')
+    const numPop2 = document.querySelector('.num_chk02')
+    hp4Btn.addEventListener('click', function () {
 
-        if(hp4Value.value == randomNum){
-            alert('맞음')
-           
-        }else{
-            alert('틀림')
+        if (hp4Value.value == randomNum) {
+            numPop.style.display = 'block'
+            numPop2.style.display = 'none'
+
+
+        } else {
+            numPop.style.display = 'none'
+            numPop2.style.display = 'block'
         }
 
     })
+
+
+
+    const myId = 'woojin'
+    const myPw = 'woojin123'
+
+    const userId = document.getElementById('name')
+    const userPw = document.getElementById('passwd')
+    const loginBtn = document.getElementById('login_btn')
+    const joinBtn = document.getElementById('join_btn')
+    const loginPopup = document.querySelector('.login_popup')
+    const loginPopup2 = document.querySelector('.login_popup2')
+    const loginPopup3 = document.querySelector('.login_popup3')
+
+   
+    
 
 
 
