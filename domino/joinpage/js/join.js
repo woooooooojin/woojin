f2aa8213101e0fcfa6c5e929221f8f5f8c1c8111
woojin <dnwls5995@naver.com>
2023-05-11 15:50:13 +0900

    0511

---
+++ domino/joinpage/js/join.js
@@ -0,0 +1,38 @@
+
+document.addEventListener('DOMContentLoaded',function(){
+
+
+    const userId = document.getElementById('user_id')
+    const userPw1 = document.getElementById('user_pw1')
+    const userPw2 = document.getElementById('user_pw2')
+
+
+
+
+    userId.addEventListener('change', checkId)
+    userPw1.addEventListener('change', checkPw)
+    userPw2.addEventListener('chage', comparePW)
+
+    function checkId(){
+
+
+    }//check id ()
+
+
+
+    
+
+    function checkPw(){
+
+
+    }//checkpw ()
+
+
+
+
+    function comparePW(){
+
+
+    }//compare pw()
+
+})
