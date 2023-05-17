7635972c75fabac75d2eaef82e018c3aa68ba39d
woooooooojin <kevin2205@naver.com>
2023-05-17 21:55:07 +0900

    0517

--- fila_team/js/detail.js
+++ fila_team/js/detail.js
@@ -76,5 +76,124 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+    //리뷰 등록
+
+    const reviewBtn = document.getElementById('review_btn')//리뷰버튼
+    const reviewName = document.getElementById('rename')//리뷰아이디
+    const reviewTxt = document.getElementById('retxt')//리뷰본문
+    const R_List = document.querySelector('.R_list')
+
+
+    reviewBtn.addEventListener('click',function(){
+
+        const Li = document.createElement('li')
+        Li.setAttribute('class','review_inner')
+
+        const numberBox = document.createElement('div')
+        numberBox.setAttribute('class','inner_number')
+        Li.appendChild(numberBox)
+
+        const numberSpan = document.createElement('span')
+        numberSpan.setAttribute('class','review_txt')
+        numberBox.appendChild(numberSpan)
+
+        let Rcnt =1;
+        function plusCnt(){
+            
+                Rcnt++
+          
+            
+        }numberSpan.innerHTML = Rcnt
+        plusCnt()
+        
+        
+        
+        
+      
+
+        const starBox = document.createElement('div')
+        starBox.setAttribute('class','inner_stars')
+        Li.appendChild(starBox)
+
+
+        // 별
+        const star1 = document.createElement('i')
+        star1.setAttribute('class','fas fa-star')
+        starBox.appendChild(star1)
+
+        const star2 = document.createElement('i')
+        star2.setAttribute('class','fas fa-star')
+        starBox.appendChild(star2)
+        const star3 = document.createElement('i')
+        star3.setAttribute('class','fas fa-star')
+        starBox.appendChild(star3)
+        const star4 = document.createElement('i')
+        star4.setAttribute('class','fas fa-star')
+        starBox.appendChild(star4)
+        const star5 = document.createElement('i')
+        star5.setAttribute('class','fas fa-star')
+        starBox.appendChild(star5)
+
+
+        const txtBox = document.createElement('div')
+        txtBox.setAttribute('class','inner_txt')
+        Li.appendChild(txtBox)
+
+        const txtspan = document.createElement('span')
+        txtspan.setAttribute('class','txt_span')
+        txtBox.appendChild(txtspan)
+        txtspan.innerHTML = reviewTxt.value
+        reviewTxt.value = ''
+        
+
+
+        const idBox = document.createElement('div')
+        idBox.setAttribute('class','inner_id')
+        Li.appendChild(idBox)
+
+        const idSpan = document.createElement('span')
+        idSpan.setAttribute('class','id_span')
+        idBox.appendChild(idSpan)
+        idSpan.innerHTML = reviewName.value
+        reviewName.value=''
+
+        const dateBox = document.createElement('div')
+        dateBox.setAttribute('class','inner_date')
+        Li.appendChild(dateBox)
+
+        const dateSpan = document.createElement('span')
+        dateSpan.setAttribute('class','date_span')
+        dateBox.appendChild(dateSpan)
+        
+        //날짜 넣기
+        let nowDate = new Date().toLocaleDateString();
+          dateSpan.innerHTML = nowDate
+
+
+        const closeBox = document.createElement('div')
+        closeBox.setAttribute('class','inner_close')
+        Li.appendChild(closeBox)
+
+        const closeX = document.createElement('i')
+        closeX.setAttribute('class','fas fa-times')
+        closeBox.appendChild(closeX)
+
+        
+
+
+
+        R_List.appendChild(Li)
+
+        closeX.addEventListener('click',function(){
+            R_List.removeChild(Li)
+        })
+       
+
+
+
+
+    })
+   
+
 
 })
