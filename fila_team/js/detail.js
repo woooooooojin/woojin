25592dba50b14739d67969516fb761338755d931
woojin <dnwls5995@naver.com>
2023-05-18 15:49:54 +0900

    0518

--- fila_team/js/detail.js
+++ fila_team/js/detail.js
@@ -78,130 +78,130 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //리뷰 등록
 
-    const reviewBtn = document.getElementById('review_btn')//리뷰버튼
-    const reviewName = document.getElementById('rename')//리뷰아이디
-    const reviewTxt = document.getElementById('retxt')//리뷰본문
+    const reviewBtn = document.getElementById('review_btn') //리뷰버튼
+    const reviewName = document.getElementById('rename') //리뷰아이디
+    const reviewTxt = document.getElementById('retxt') //리뷰본문
     const R_List = document.querySelector('.R_list')
-    
 
-    reviewBtn.addEventListener('click',function(){
+
+    reviewBtn.addEventListener('click', function () {
 
         const Li = document.createElement('li')
-        Li.setAttribute('class','review_inner')
+        Li.setAttribute('class', 'review_inner')
 
         const numberBox = document.createElement('div')
-        numberBox.setAttribute('class','inner_number')
+        numberBox.setAttribute('class', 'inner_number')
         Li.appendChild(numberBox)
 
         const numberSpan = document.createElement('span')
-        numberSpan.setAttribute('class','review_txt')
+        numberSpan.setAttribute('class', 'review_txt')
         numberBox.appendChild(numberSpan)
 
-        let Rcnt =1;
+        let Rcnt = 1;
         const Lis = document.querySelectorAll('.review_inner')
 
-        function plusCnt(){
-            for(i=0;i<Lis.length;i++){
+        function plusCnt() {
+            for (i = 0; i < Lis.length; i++) {
                 Rcnt++
             }
-                
+
             numberSpan.innerHTML = Rcnt
-            
+
         }
         plusCnt()
-        
-        
-        
-        
-      
+
+
+
+
+
 
         const starBox = document.createElement('div')
-        starBox.setAttribute('class','inner_stars')
+        starBox.setAttribute('class', 'inner_stars')
         Li.appendChild(starBox)
 
 
         // 별
         const star1 = document.createElement('i')
-        star1.setAttribute('class','fas fa-star')
+        star1.setAttribute('class', 'fas fa-star')
         starBox.appendChild(star1)
 
         const star2 = document.createElement('i')
-        star2.setAttribute('class','fas fa-star')
+        star2.setAttribute('class', 'fas fa-star')
         starBox.appendChild(star2)
         const star3 = document.createElement('i')
-        star3.setAttribute('class','fas fa-star')
+        star3.setAttribute('class', 'fas fa-star')
         starBox.appendChild(star3)
         const star4 = document.createElement('i')
-        star4.setAttribute('class','fas fa-star')
+        star4.setAttribute('class', 'fas fa-star')
         starBox.appendChild(star4)
         const star5 = document.createElement('i')
-        star5.setAttribute('class','fas fa-star')
+        star5.setAttribute('class', 'fas fa-star')
         starBox.appendChild(star5)
 
 
         const txtBox = document.createElement('div')
-        txtBox.setAttribute('class','inner_txt')
+        txtBox.setAttribute('class', 'inner_txt')
         Li.appendChild(txtBox)
 
         const txtspan = document.createElement('span')
-        txtspan.setAttribute('class','txt_span')
+        txtspan.setAttribute('class', 'txt_span')
         txtBox.appendChild(txtspan)
         txtspan.innerHTML = reviewTxt.value
         reviewTxt.value = ''
-        
+
 
 
         const idBox = document.createElement('div')
-        idBox.setAttribute('class','inner_id')
+        idBox.setAttribute('class', 'inner_id')
         Li.appendChild(idBox)
 
         const idSpan = document.createElement('span')
-        idSpan.setAttribute('class','id_span')
+        idSpan.setAttribute('class', 'id_span')
         idBox.appendChild(idSpan)
         idSpan.innerHTML = reviewName.value
-        reviewName.value=''
+        reviewName.value = ''
 
         const dateBox = document.createElement('div')
-        dateBox.setAttribute('class','inner_date')
+        dateBox.setAttribute('class', 'inner_date')
         Li.appendChild(dateBox)
 
         const dateSpan = document.createElement('span')
-        dateSpan.setAttribute('class','date_span')
+        dateSpan.setAttribute('class', 'date_span')
         dateBox.appendChild(dateSpan)
-        
+
         //날짜 넣기
         let nowDate = new Date().toLocaleDateString();
-          dateSpan.innerHTML = nowDate
+        dateSpan.innerHTML = nowDate
 
 
         const closeBox = document.createElement('div')
-        closeBox.setAttribute('class','inner_close')
+        closeBox.setAttribute('class', 'inner_close')
         Li.appendChild(closeBox)
 
         const closeX = document.createElement('i')
-        closeX.setAttribute('class','fas fa-times')
+        closeX.setAttribute('class', 'fas fa-times')
         closeBox.appendChild(closeX)
 
-        
 
 
 
-       
 
-     
-        
+
+
+
+
         R_List.appendChild(Li)
 
-        closeX.addEventListener('click',function(){
+        closeX.addEventListener('click', function () {
             R_List.removeChild(Li)
         })
-       
+
 
 
 
 
     })
-   
+
 
 
 
@@ -212,32 +212,36 @@ document.addEventListener('DOMContentLoaded', function () {
     const plusBox = document.querySelector('.plus_box')
     const cntBox = document.getElementById('cnt_box')
     const totalPrice = document.querySelector('.total_price span')
-    
-    minusBox.addEventListener('click',function(){
 
-        cntBox.value --
-        if(cntBox.value <= 0 ){
+    minusBox.addEventListener('click', function () {
+
+        cntBox.value--
+        if (cntBox.value <= 0) {
             cntBox.value = 1
         }
         let result = cntBox.value * 119000
         totalPrice.innerHTML = result.toLocaleString('ko-KR')
         // totalPrice.innerHTML = cntBox.value * 119000
     })
-    plusBox.addEventListener('click',function(){
+    plusBox.addEventListener('click', function () {
 
-        cntBox.value ++
+        cntBox.value++
+        if (cntBox.value > 20) {
+            alert('수량은 최대 20개 가능합니다.')
+            cntBox.value = '20'
+        }
         let result = cntBox.value * 119000
         totalPrice.innerHTML = result.toLocaleString('ko-KR')
         // totalPrice.innerHTML = cntBox.value * 119000
 
     })
 
-    cntBox.addEventListener('change',function(){
+    cntBox.addEventListener('change', function () {
         let result = cntBox.value * 119000
         totalPrice.innerHTML = result.toLocaleString('ko-KR')
 
-        if(cntBox.value <= 0){
-            cntBox.value='1'
+        if (cntBox.value <= 0) {
+            cntBox.value = '1'
             alert('수량은 1개이상 가능합니다.')
             totalPrice.innerHTML = '119,000'
         }
@@ -245,7 +249,90 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-    
+
+
+    //상품문의 등록
+    const noticeName = document.getElementById('notice_name')
+    const noticeTxt = document.getElementById('notice_txt')
+    const noticeBtn = document.getElementById('notice_btn')
+    const noticeList = document.querySelector('.N_list')
+
+    noticeBtn.addEventListener('click', function () {
+
+        const noticeLi = document.createElement('li')
+        noticeLi.setAttribute('class', 'notice_inner')
+
+        const noticeNum = document.createElement('div')
+        noticeNum.setAttribute('class', 'notice_num')
+        noticeLi.appendChild(noticeNum)
+
+        const noticeNumSpan = document.createElement('span')
+        noticeNumSpan.setAttribute('class', 'notice_num_span')
+        noticeNum.appendChild(noticeNumSpan)
+
+        let Rcnt = 1;
+        const noLis = document.querySelectorAll('.notice_inner')
+
+        function plusCnt() {
+            for (i = 0; i < noLis.length; i++) {
+                Rcnt++
+            }
+
+            noticeNumSpan.innerHTML = Rcnt
+
+        }
+        plusCnt()
+
+        const noticeText = document.createElement('div')
+        noticeText.setAttribute('class','notice_txt')
+        noticeLi.appendChild(noticeText)
+
+        const textSpan = document.createElement('span')
+        textSpan.setAttribute('class','notice_text_span')
+        noticeText.appendChild(textSpan)
+
+        textSpan.innerHTML = noticeTxt.value
+        noticeTxt.value = ''
+
+        const noticeId = document.createElement('div')
+        noticeId.setAttribute('class','notice_id')
+        noticeLi.appendChild(noticeId)
+
+        const noticeIdSpan = document.createElement('span')
+        noticeIdSpan.setAttribute('class','notice_id_span')
+        noticeId.appendChild(noticeIdSpan)
+
+        noticeIdSpan.innerHTML = noticeName.value
+        noticeName.value = ''
+
+        const noticeDate = document.createElement('div')
+        noticeDate.setAttribute('class','notice_date')
+        noticeLi.appendChild(noticeDate)
+
+        const noticeDateSpan = document.createElement('span')
+        noticeDateSpan.setAttribute('class','notice_date_span')
+        noticeDate.appendChild(noticeDateSpan)
+
+        let nowNoticeDate = new Date().toLocaleDateString();
+        noticeDateSpan.innerHTML = nowNoticeDate
+
+        const noticeClose = document.createElement('div')
+        noticeClose.setAttribute('class','notice_close')
+        noticeLi.appendChild(noticeClose)
+
+        const noticeI = document.createElement('i')
+        noticeI.setAttribute('class','fas fa-times')
+        noticeClose.appendChild(noticeI)
+
+
+
+
+        noticeList.appendChild(noticeLi)
+
+        noticeClose.addEventListener('click', function () {
+            noticeList.removeChild(noticeLi)
+        })
+    })
 
 
 
@@ -257,6 +344,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-    
+
+
+
+
+
+
+
 
 })
