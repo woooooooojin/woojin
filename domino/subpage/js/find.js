642e8792b3fc84a51ab76045071b1012f3bbdb04
woooooooojin <kevin2205@naver.com>
2023-05-18 22:17:46 +0900

    0518

--- domino/subpage/js/find.js
+++ domino/subpage/js/find.js
@@ -1,11 +1,50 @@
-import membership from './membership.js'
+
+
+
+const membership = [
+    {
+        id : "member01",
+        pw :"hong1234",
+        name : "홍길동",
+        birth : '900301',
+    },
+    {
+        id: "member02",
+        name: "이순신",
+        pw:"lee1234",
+        birth : '910501',
+    },
+    {
+        id: "member03",
+        name: "김구",
+        pw:"kim1234",
+        birth : '890501',
+    },
+    {
+        id: "member04",
+        name: "윤봉길",
+        pw:"yoon1234",
+        birth : '680903',
+    },
+    {
+        id: "woojin",
+        name: "최우진",
+        pw:"woojin123",
+        birth : '940402',
+    },
+    
+    
+    
+    
+    
+    ]
+
+
 
 const findName = document.getElementById('findid')
 const findId = document.getElementById('findpw')
 const findNum01= document.getElementById('findnum01')
 const findNum02= document.getElementById('findnum02')
-const myId = document.querySelector('.idbox')
-const myPw = document.querySelector('.pwbox')
 const idBtn = document.getElementById('idbtn')
 const pwBtn = document.getElementById('pwbtn')
 
@@ -16,10 +55,7 @@ pwBtn.addEventListener('click',yourPw)
 function yourId() {
     for (let i = 0; i < membership.length; i++) {
         if (findName.value === membership[i].name && findNum01.value === membership[i].birth) {
-            myId.innerHTML = '아이디는 ' + membership[i].id + ' 입니다.' + membership[i].name
- 
-        }else{
-            myId.innerHTML = '정보를 확인해 주세요.'
+            alert('아이디는 '+(JSON.stringify(membership[i].id))+' 입니다.')
         }
 
 
@@ -31,10 +67,7 @@ function yourPw(){
 
     for(let i =0; i < membership.length; i++){
         if(findNum02.value === membership[i].birth && findId.value === membership[i].id){
-            myPw.innerHTML = '비밀번호는 ' + membership[i].pw + ' 입니다.'
-        }else{
-            myPw.innerHTML = '정보를 확인해 주세요.'
-            
+           alert('비밀번호는 ' + (JSON.stringify(membership[i].pw )) + ' 입니다.')
         }
 
     }
