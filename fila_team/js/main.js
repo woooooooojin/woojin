1e15c7327dea1891a2e259102f8c2c83b93f5364
woojin <dnwls5995@naver.com>
2023-05-22 15:49:52 +0900

    0522

--- fila_team/js/main.js
+++ fila_team/js/main.js
@@ -271,3 +271,147 @@ document.addEventListener('DOMContentLoaded',()=>{
         searchBox.style.display = 'block'
     })
 })
+
+
+
+///////////////////랭킹스와이퍼 카테고리 클릭이벤트//////////////////////////
+
+const tshirt = document.getElementById('tshirt')
+const online = document.getElementById('online')
+const targa = document.getElementById('targa')
+const slides = document.querySelectorAll('.rank_slide img')
+
+const rankTit = document.querySelector('.rank_text h4')
+const rankList = document.querySelectorAll('.ranklist li')
+const tennis = document.getElementById('tennis')
+const running = document.getElementById('running')
+
+tshirt.addEventListener('click',function(){
+
+    slides[0].setAttribute('src','./img/swiper_img/01.jpg')
+    slides[1].setAttribute('src','./img/swiper_img/02.jpg')
+    slides[2].setAttribute('src','./img/swiper_img/03.jpg')
+    slides[3].setAttribute('src','./img/swiper_img/04.jpg')
+    slides[4].setAttribute('src','./img/swiper_img/05.jpg')
+    slides[5].setAttribute('src','./img/swiper_img/06.jpg')
+
+    rankTit.innerHTML = '#티셔츠 & 팬츠'
+    const tarArr = ['01 컴포트핏 인터네셔널 반팔 맨투맨','02 컴포트핏 조거팬츠','03 이지팬츠','04 베이직 리니어 반팔티','05 3D 빅로고 반팔티','06 컴포트핏 인터네셔널 로고 반팔티']
+    for(let i in tarArr){
+        for(let i =0; i<rankList.length; i++){
+            rankList[i].innerHTML = tarArr[i]
+        }
+    }
+
+
+})
+targa.addEventListener('click',function(){
+
+    slides[0].setAttribute('src','./img/swiper_img/2-01.jpg')
+    slides[1].setAttribute('src','./img/swiper_img/2-02.jpg')
+    slides[2].setAttribute('src','./img/swiper_img/2-03.jpg')
+    slides[3].setAttribute('src','./img/swiper_img/2-04.jpg')
+    slides[4].setAttribute('src','./img/swiper_img/2-05.jpg')
+    slides[5].setAttribute('src','./img/swiper_img/2-06.jpg')
+    rankTit.innerHTML = '#타르가'
+
+    const tarArr = ['타르가01','타르가02','타르가03','타르가04','타르가05','타르가06']
+    for(let i in tarArr){
+        for(let i =0; i<rankList.length; i++){
+            rankList[i].innerHTML = tarArr[i]
+        }
+    }
+
+})
+
+online.addEventListener('click',function(){
+
+    slides[0].setAttribute('src','./img/swiper_img/3-01.jpg')
+    slides[1].setAttribute('src','./img/swiper_img/3-02.jpg')
+    slides[2].setAttribute('src','./img/swiper_img/3-03.jpg')
+    slides[3].setAttribute('src','./img/swiper_img/3-04.jpg')
+    slides[4].setAttribute('src','./img/swiper_img/3-05.jpg')
+    slides[5].setAttribute('src','./img/swiper_img/3-06.jpg')
+    rankTit.innerHTML = '#온라인단독'
+
+    
+    const tarArr = ['온라인01','온라인02','온라인03','온라인04','온라인05','온라인06']
+    for(let i in tarArr){
+        for(let i =0; i<rankList.length; i++){
+            rankList[i].innerHTML = tarArr[i]
+        }
+    }
+})
+
+tennis.addEventListener('click',function(){
+    slides[0].setAttribute('src','./img/swiper_img/4-01.jpg')
+    slides[1].setAttribute('src','./img/swiper_img/4-02.jpg')
+    slides[2].setAttribute('src','./img/swiper_img/4-03.jpg')
+    slides[3].setAttribute('src','./img/swiper_img/4-04.jpg')
+    slides[4].setAttribute('src','./img/swiper_img/4-05.jpg')
+    slides[5].setAttribute('src','./img/swiper_img/4-06.jpg')
+    rankTit.innerHTML = '#테니스'
+
+    const tarArr = ['테니스01','테니스02','테니스3','테니스04','테니스05','테니스06']
+    for(let i in tarArr){
+        for(let i =0; i<rankList.length; i++){
+            rankList[i].innerHTML = tarArr[i]
+        }
+    }
+
+})
+
+running.addEventListener('click',function(){
+    slides[0].setAttribute('src','./img/swiper_img/5-01.jpg')
+    slides[1].setAttribute('src','./img/swiper_img/5-02.jpg')
+    slides[2].setAttribute('src','./img/swiper_img/5-03.jpg')
+    slides[3].setAttribute('src','./img/swiper_img/5-04.jpg')
+    slides[4].setAttribute('src','./img/swiper_img/5-05.jpg')
+    slides[5].setAttribute('src','./img/swiper_img/5-06.jpg')
+    rankTit.innerHTML = '#런닝화'
+
+    const tarArr = ['러닝01','러닝02','러닝03','러닝04','러닝05','러닝06']
+    for(let i in tarArr){
+        for(let i =0; i<rankList.length; i++){
+            rankList[i].innerHTML = tarArr[i]
+        }
+    }
+})
+
+
+
+
+//////////////신제품 카테고리 클릭 이벤트//////////////////
+
+const newTshirt = document.getElementById('newTshirt')
+const newTennis = document.getElementById('newTennis')
+const newShoes = document.getElementById('newShoes')
+const newSlide = document.querySelectorAll('.newslide img')
+
+const newTit = document.querySelectorAll('.new_text h4')
+const newFirstP = document.querySelectorAll('.new_text .first_p')
+const newSecondP = document.querySelectorAll('.new_text .second_p')
+const newDetail = document.querySelectorAll('.new_text .new_detail')
+
+newTshirt.addEventListener('click',function(){
+
+    newSlide[0].setAttribute('src','./img/newarrival/new04.jpg')
+    newSlide[1].setAttribute('src','./img/newarrival/new05.jpg')
+    newSlide[2].setAttribute('src','./img/newarrival/new06.jpg')
+    newSlide[3].setAttribute('src','./img/newarrival/new01.jpg')
+    newSlide[4].setAttribute('src','./img/newarrival/new02.jpg')
+    newSlide[5].setAttribute('src','./img/newarrival/new03.jpg')
+
+    const newTitArr = ['테니스짐백','휠라 테이퍼 샌들 V3','컴포트핏<br/>우븐 후드티','테니스 반바지','컴포트핏 플래그 반팔티']
+    for(let i in newTitArr){
+        for(let i =0; i<newSlide.length; i++){
+            newTit[i] = newTitArr[i]
+        }
+    }
+
+    
+
+})
+
+
+
