c6b97abcfdfe25b83f1226fe95c372b24defb574
woojin <kevin2205@naver.com>
2023-05-31 17:50:15 +0900

    0531

--- fila_team/js/sub01.js
+++ fila_team/js/sub01.js
@@ -158,8 +158,9 @@ document.addEventListener('DOMContentLoaded', () => {
 const subList = document.querySelector('.sublist')
 
 
-function madeDiv() {
-    for (let i = 0; i < subData.length; i++) {
+
+function madeDiv(val) {
+    for (let i = 0; i < val.length; i++) {
 
         const subDiv = document.createElement('div')
         subDiv.setAttribute('class', 'subBox')
@@ -207,7 +208,7 @@ function madeDiv() {
         linkA.setAttribute('class', 'sub_link')
         //img
         const subImg = document.createElement('img')
-        subImg.setAttribute('src', subData[i].src)
+        subImg.setAttribute('src', val[i].src)
         subImgWrap.appendChild(linkA)
         subImg.setAttribute('class', 'subimg')
         linkA.appendChild(subImg)
@@ -228,21 +229,21 @@ function madeDiv() {
 
 
         const itemCategory = document.createElement('p')
-        const itemCategoryTxt = document.createTextNode(subData[i].category)
+        const itemCategoryTxt = document.createTextNode(val[i].category)
         itemCategory.appendChild(itemCategoryTxt)
         itemDescWrap.appendChild(itemCategory)
         itemCategory.setAttribute('class', 'item_category')
 
 
         const itemName = document.createElement('p')
-        const itemNameTxt = document.createTextNode(subData[i].name)
+        const itemNameTxt = document.createTextNode(val[i].name)
         itemName.appendChild(itemNameTxt)
         itemDescWrap.appendChild(itemName)
         itemName.setAttribute('class', 'item_name')
 
 
         const itemPrice = document.createElement('p')
-        const itemPriceTxt = document.createTextNode(subData[i].price)
+        const itemPriceTxt = document.createTextNode(val[i].price)
         itemPrice.appendChild(itemPriceTxt)
         itemDescWrap.appendChild(itemPrice)
         itemPrice.setAttribute('class', 'item_price')
@@ -274,110 +275,129 @@ function madeDiv() {
 
 
 }
-madeDiv()
+// madeDiv(subData)
 
 
 ////////////////////pagenation/////////////////////////
-
-
+let nowPage = 0
 const showPerPage = 20; //화면에 보여질 개수
-const subItems = document.querySelectorAll('.subBox')
-const numOfContent = subItems.length; // 컨텐츠 개수
-const pageCount = Math.ceil(numOfContent / showPerPage); //페이지 버튼 개수
 
 
 
-let paging = document.querySelector('.paging_ex') //page 들어갈곳
-for (let i = 1; i <= pageCount; i++) {
-    paging.innerHTML += `<li><a href="#!">${i}</a></li>`
+function createPage(val) {
+    //const subItems = document.querySelectorAll('.subBox')
+    const numOfContent = val.length; // 컨텐츠 개수
+    const pageCount = Math.ceil(numOfContent / showPerPage); //페이지 버튼 개수
+    const start = nowPage * showPerPage
+    const end = start + showPerPage
+    const newSub = val.slice(start, end)
+    madeDiv(newSub)
 
-} // li 생성
 
-const pageCountBtn = paging.querySelectorAll('a') //페이지네이션 a
+    let paging = document.querySelector('.paging_ex') //page 들어갈곳
+    for (let i = 1; i <= pageCount; i++) {
+        paging.innerHTML += `<li><a href="#!">${i}</a></li>`
 
-pageCountBtn.forEach((item, idx) => {
-    item.addEventListener('click', (e) => {
-        e.preventDefault()
+    } // li 생성
 
-        //출력
-        displayItem(idx);
+    const pageCountBtn = paging.querySelectorAll('a') //페이지네이션 a
+
+    pageCountBtn.forEach((item, idx) => {
+        item.addEventListener('click', () => {
+            for (let value of pageCountBtn) {
+                value.classList.remove('active')
+            }
+            if (nowPage !== idx) {
+                nowPage = idx
+                paging.innerHTML = ''
+
+                sublist.innerHTML = ''
+                createPage(val)
+            }
+
+
+
+        })
+
+        // //맨앞 맨뒤 페이지
+        const prevBtn = document.querySelector('.prev_btn')
+        const nextBtn = document.querySelector('.next_btn')
+        // const lastpage = pageCount
+        console.log(pageCount)
+        prevBtn.addEventListener('click', function () {
+            nowPage = 0
+            paging.innerHTML = ''
+            sublist.innerHTML = ''
+            createPage(val)
+        })
+        nextBtn.addEventListener('click', function () {
+            nowPage = pageCount - 1
+            paging.innerHTML = ''
+            sublist.innerHTML = ''
+            createPage(val)
+        })
+        if (nowPage === idx) {
+            pageCountBtn[idx].classList.add('active')
+
+        }
+
     })
 
-    //맨앞 맨뒤 페이지
-    const prevBtn = document.querySelector('.prev_btn')
-    const nextBtn = document.querySelector('.next_btn')
-    prevBtn.addEventListener('click', function () {
-        displayItem(0)
-    })
-    nextBtn.addEventListener('click', function () {
-        displayItem(idx)
-    })
-
-
-})
-
-
-
-function displayItem(idx) {
-    let start = idx * showPerPage // 20개 기준 idx = 0 * 20 = 0, idx=1 * 20 =20 ,,, 
-    let end = start + showPerPage // 20개 기준 start = 20, 40 , 60
-    let subArr = [...subItems] //새로운 배열로 만듬 console에 찍히는 nodelist는 slice 못씀
-    for (let val of subArr) {
-        val.style.display = 'none'
-    }
-
-    let newSub = subArr.slice(start, end) //새로운 배열로 만듬, start ~ end 구간 slice
-    for (let val of newSub) {
-        val.style.display = ''
-    }
-    for (let value of pageCountBtn) {
-        value.classList.remove('active')
-
-    }
-    pageCountBtn[idx].classList.add('active')
 
 
 }
-displayItem(0)
-
-
+createPage(subData)
 
 //////////////////select 필터////////////////////////////
 
 const selectBox = document.getElementById('select_sort'); //select box
 const sublist = document.querySelector('.sublist')
-
+let paging = document.querySelector('.paging_ex')
 selectBox.addEventListener('change', function () {
     if (selectBox.value === 'expensive') {
-        subData.sort((a, b) => {
+        let exp = subData.sort((a, b) => {
             return (a.price01 - b.price01) * -1
         })
         sublist.innerHTML = ''
-        madeDiv() //높은가격순
+        paging.innerHTML = ''
+        nowPage = 0
+        createPage(exp)
+
+
     } else if (selectBox.value === 'cheap') {
-        subData.sort((a, b) => {
+        let cheap = subData.sort((a, b) => {
             return (a.price01 - b.price01)
         })
         sublist.innerHTML = ''
-        madeDiv() //낮은가격순
+        paging.innerHTML = ''
+        nowPage = 0
+        createPage(cheap)
+        //낮은가격순
     } else if (selectBox.value === 'basic') {
-        subData.sort((a, b) => {
+        let basic = subData.sort((a, b) => {
             return (a.id - b.id)
         })
         sublist.innerHTML = ''
-        madeDiv() //기본순
+        paging.innerHTML = ''
+        nowPage = 0
+        createPage(basic)
+        // 기본
     } else if (selectBox.value === 'pop') {
-        subData.sort((a, b) => {
+        let pop = subData.sort((a, b) => {
             return (a.pop - b.pop)
         })
         sublist.innerHTML = ''
-        madeDiv() //인기순
+        paging.innerHTML = ''
+        nowPage = 0
+        createPage(pop) //인기순
     } else if (selectBox.value === 'new') {
-        subData.sort((a, b) => {
+        let newarrival = subData.sort((a, b) => {
             return new Date(a.day).getTime() - new Date(b.day).getTime()
         }) //신상날짜순
         sublist.innerHTML = ''
-        madeDiv() //신상순
+        paging.innerHTML = ''
+        nowPage = 0
+        createPage(newarrival) //신상순
     }
 
 })
@@ -399,112 +419,117 @@ const priceChk04 = document.getElementById('price_chk04')
 const priceChk05 = document.getElementById('price_chk05')
 const priceChkDefault = document.getElementById('price_chk06')
 
+const bk = subData.filter((a) => {
+    return a.color === 'black'
+})
 
+const gr = subData.filter((a) => {
+    return a.color === 'gray'
+})
+
+const wht = subData.filter((a) => {
+    return a.color === 'white'
+})
+const price01 = subData.filter((a) => {
+    return a.price01 <= 39000
+})
+const price02 = subData.filter((a) => {
+    return a.price01 >= 39000 && a.price01 <= 49000
+})
+const price03 = subData.filter((a) => {
+    return a.price01 >= 49000 && a.price01 <= 59000
+})
+const price04 = subData.filter((a) => {
+    return a.price01 >= 59000 && a.price01 <= 79000
+})
+const price05 = subData.filter((a) => {
+    return a.price01 >= 79000
+})
 
 
 
 blackColor.addEventListener('click', function () {
-    for (let i = 0; i < subData.length; i++) {
-        if (subData[i].color === 'black') {
-            itemlist[i].style.display = ''
-        } else {
-            itemlist[i].style.display = 'none'
-        }
-    }
+    sublist.innerHTML = ''
+    paging.innerHTML = ''
+    nowPage = 0
+    createPage(bk)
 
 
 })
 
 whiteColor.addEventListener('click', function () {
-    for (let i = 0; i < subData.length; i++) {
-        if (subData[i].color === 'white') {
-            itemlist[i].style.display = ''
-        } else {
-            itemlist[i].style.display = 'none'
-        }
-    }
+    sublist.innerHTML = ''
+    paging.innerHTML = ''
+    nowPage = 0
+    createPage(wht)
+
 
 })
 grayColor.addEventListener('click', function () {
-    for (let i = 0; i < subData.length; i++) {
-        if (subData[i].color === 'gray') {
-            itemlist[i].style.display = ''
-        } else {
-            itemlist[i].style.display = 'none'
-        }
-    }
+    sublist.innerHTML = ''
+    paging.innerHTML = ''
+    nowPage = 0
+    createPage(gr)
 
 
 })
 
 defaultVal.addEventListener('click', function () {
     sublist.innerHTML = ''
-    madeDiv()
+    paging.innerHTML = ''
+    nowPage = 0
+    createPage(subData)
 
 })
 
 priceChk01.addEventListener('click', function () {
-    for (let i = 0; i < subData.length; i++) {
-        if (subData[i].price01 <= 39000) {
-            itemlist[i].style.display = ''
-        } else {
-            itemlist[i].style.display = 'none'
-        }
-    }
+    sublist.innerHTML = ''
+    paging.innerHTML = ''
+    nowPage = 0
+    createPage(price01)
 
 
 })
 
 priceChk02.addEventListener('click', function () {
-    for (let i = 0; i < subData.length; i++) {
-        if (subData[i].price01 >= 39000 && subData[i].price01 <= 49000) {
-            itemlist[i].style.display = ''
-        } else {
-            itemlist[i].style.display = 'none'
-        }
-    }
+    sublist.innerHTML = ''
+    paging.innerHTML = ''
+    nowPage = 0
+    createPage(price02)
 
 
 })
 
 priceChk03.addEventListener('click', function () {
-    for (let i = 0; i < subData.length; i++) {
-        if (subData[i].price01 >= 49000 && subData[i].price01 <= 59000) {
-            itemlist[i].style.display = ''
-        } else {
-            itemlist[i].style.display = 'none'
-        }
-    }
-
+    sublist.innerHTML = ''
+    paging.innerHTML = ''
+    nowPage = 0
+    createPage(price03)
 
 })
 
 priceChk04.addEventListener('click', function () {
-    for (let i = 0; i < subData.length; i++) {
-        if (subData[i].price01 >= 59000 && subData[i].price01 <= 79000) {
-            itemlist[i].style.display = ''
-        } else {
-            itemlist[i].style.display = 'none'
-        }
-    }
+    sublist.innerHTML = ''
+    paging.innerHTML = ''
+    nowPage = 0
+    createPage(price04)
 
 
 })
 
 priceChk05.addEventListener('click', function () {
-    for (let i = 0; i < subData.length; i++) {
-        if (subData[i].price01 >= 79000) {
-            itemlist[i].style.display = ''
-        } else {
-            itemlist[i].style.display = 'none'
-        }
-    }
+    sublist.innerHTML = ''
+    paging.innerHTML = ''
+    nowPage = 0
+    createPage(price05)
 
 
 })
 priceChkDefault.addEventListener('click', function () {
     sublist.innerHTML = ''
-    madeDiv()
+    paging.innerHTML = ''
+    nowPage = 0
+    createPage(subData)
 
 })
 
