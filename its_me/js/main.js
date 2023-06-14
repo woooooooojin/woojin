d754cfc5f1ccc7018ef37b96b87514c47252f572
woojin <kevin2205@naver.com>
2023-06-14 14:45:37 +0900

    0614

--- its_me/js/main.js
+++ its_me/js/main.js
@@ -71,3 +71,49 @@ for (let i = 0; i < 300; i++) {
 
 
 
+
+const btn01 = document.getElementById('btn01')
+const btn02 = document.getElementById('btn02')
+const btn03 = document.getElementById('btn03')
+const btn04 = document.getElementById('btn04')
+const btn05 = document.getElementById('btn05')
+const section01 = document.getElementById('main')
+const section02 = document.getElementById('about')
+const section03 = document.getElementById('cardflip')
+const section04 = document.getElementById('portfolio')
+const section05 = document.getElementById('contact')
+
+
+btn01.addEventListener('click',()=>{
+    gsap.to(window,0.3,{
+        scrollTo: section01,
+    })
+
+})
+
+btn02.addEventListener('click',()=>{
+    gsap.to(window,0.3,{
+        scrollTo: section02,
+    })
+
+})
+
+btn03.addEventListener('click',()=>{
+    gsap.to(window,0.3,{
+        scrollTo: section03,
+    })
+
+})
+
+btn04.addEventListener('click',()=>{
+    gsap.to(window,0.3,{
+        scrollTo: section04,
+    })
+
+})
+btn05.addEventListener('click',()=>{
+    gsap.to(window,0.3,{
+        scrollTo: section05,
+    })
+
+})
