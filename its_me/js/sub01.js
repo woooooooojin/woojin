d6dc790a30f5089d00cff29a74e5a1b888594b44
woooooooojin <kevin2205@naver.com>
2023-06-20 20:54:59 +0900

    0620

--- its_me/js/sub01.js
+++ its_me/js/sub01.js
@@ -25,121 +25,74 @@ const swiperBox = document.querySelector('.swiperbox')
 gsap.from(sortBox, 0.3, {
     scrollTrigger: {
         trigger: skillsTools,
-        start: 'bottom 50%',
+        start: 'bottom 30%',
     },
     y: 50,
     opacity: 0,
-    
+
 })
 
 gsap.from(filterBox, 0.3, {
     scrollTrigger: {
         trigger: sortBox,
-        start: 'bottom 50%',
+        start: 'bottom 30%',
     },
     y: 50,
     opacity: 0,
-    
+
 })
 gsap.from(reviewBox, 0.3, {
     scrollTrigger: {
         trigger: filterBox,
-        start: 'bottom 50%',
+        start: 'bottom 30%',
     },
     y: 50,
     opacity: 0,
-    
+
 })
 gsap.from(noticeBox, 0.3, {
     scrollTrigger: {
         trigger: reviewBox,
-        start: 'bottom 50%',
+        start: 'bottom 30%',
     },
     y: 50,
     opacity: 0,
-    
+
 })
 gsap.from(DomBox, 0.3, {
     scrollTrigger: {
         trigger: noticeBox,
-        start: 'bottom 50%',
+        start: 'bottom 30%',
     },
     y: 50,
     opacity: 0,
-    
+
 })
 gsap.from(swiperBox, 0.3, {
     scrollTrigger: {
         trigger: DomBox,
-        start: 'bottom 50%',
+        start: 'bottom 30%',
     },
     y: 50,
     opacity: 0,
-    
+
 })
 
 
 
 
+const scrollRight = document.querySelector('.scroll_right')
+const scrollLeft = document.querySelector('.scroll_left')
 
+window.addEventListener('scroll', () => {
 
+    if(window.scrollY >= 3500){
+        scrollRight.classList.add('hide1')
+        scrollLeft.classList.add('hide2')
+    }else{
+        scrollRight.classList.remove('hide1')
+        scrollLeft.classList.remove('hide2')
+    }
+   
 
-//////code popup//////// 
-
-// const popup01 = document.querySelector('.popup_01')
-// const popup02 = document.querySelector('.popup_02')
-// const popup03 = document.querySelector('.popup_03')
-// const popup04 = document.querySelector('.popup_04')
-
-// const popup01Btn = document.querySelector('.btn01')
-// const popup02Btn = document.querySelector('.btn02')
-// const popup03Btn = document.querySelector('.btn03')
-// const popup04Btn = document.querySelector('.btn04')
-
-// const closeBtn01 = document.querySelector('.cbtn01')
-// const closeBtn02 = document.querySelector('.cbtn02')
-// const closeBtn03 = document.querySelector('.cbtn03')
-// const closeBtn04 = document.querySelector('.cbtn04')
-
-// popup01Btn.addEventListener('click',()=>{
-//     popup01.style.display = 'block'
-// })
-// closeBtn01.addEventListener('click',()=>{
-//     popup01.style.display='none'
-// })
-
-// popup02Btn.addEventListener('click',()=>{
-//     popup02.style.display = 'block'
-// })
-// closeBtn02.addEventListener('click',()=>{
-//     popup02.style.display='none'
-// })
-
-// popup03Btn.addEventListener('click',()=>{
-//     popup03.style.display='block'
-// })
-// closeBtn03.addEventListener('click',()=>{
-//     popup03.style.display = 'none'
-// })
-
-// popup04Btn.addEventListener('click',()=>{
-//     popup04.style.display='block'
-// })
-// closeBtn04.addEventListener('click',()=>{
-//     popup04.style.display = 'none'
-// })
-
-
-// const popup = document.querySelectorAll('.pop')
-// const popBtn = document.querySelectorAll('.code_btn')
-// const popClose = document.querySelectorAll('.close_btn')
-
-// for(let i =0; i<popup.length; i++){
-//     popBtn[i].addEventListener('click',function(){
-//         popup[i].style.display = 'block'
-//     })
-//     popClose[i].addEventListener('click',function(){
-//         popup[i].style.display='none'
-
-//     })
-// }
+})
