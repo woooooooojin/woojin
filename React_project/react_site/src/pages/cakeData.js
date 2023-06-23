f90456073d198eb5605454cf390579171fd934a9
woojin <kevin2205@naver.com>
2023-06-23 17:48:37 +0900

    0623

---
+++ React_project/react_site/src/pages/cakeData.js
@@ -0,0 +1,56 @@
+const cakeData = [
+    {
+      id: 'cake_01',
+      image: process.env.PUBLIC_URL+'/img/cake/cake01.png',
+      desc: '찐한 카라멜과 짭쪼롬한 치즈가 어우려진 단짠의 조화!',
+      title: '딥~한 카라멜 치즈 케이크',
+      tag : '#이달의케이크 #카라멜치즈',
+      price: 18000
+    },
+    {
+        id: 'cake_02',
+        image: process.env.PUBLIC_URL+'/img/cake/cake02.png',
+        desc: '부끄러운 웰시코기의 귀여운 뒷모습을 표현한 케이크!',
+        title: '수줍은 웰시코기의 숨바꼭질',
+        tag : '#웰시코기 #수줍은케이크',
+        price: 18500
+      },
+      {
+        id: 'cake_03',
+        image: process.env.PUBLIC_URL+'/img/cake/cake03.png',
+        desc: '부끄러운 기린의 귀여운 뒷모습을 표현한 케이크!',
+        title: '수줍은 기린의 숨바꼭질',
+        tag : '#기린 #수줍은케이크',
+        price: 18500
+      },
+      {
+        id: 'cake_04',
+        image: process.env.PUBLIC_URL+'/img/cake/cake04.png',
+        desc: '부끄러운 기린의 귀여운 뒷모습을 표현한 케이크!',
+        title: '배이비 애플 타르트',
+        tag : '#애플샤베트 #애플타르트',
+        price: 19500
+      },
+      {
+        id: 'cake_05',
+        image: process.env.PUBLIC_URL+'/img/cake/cake05.png',
+        desc: '달콤한 아이스크림을 즐기러 피크닉을 떤나 구름 카페속 시나모롤!',
+        title: '시나모롤의 구름 속 피크닉',
+        tag : '#베리베리스토로베리 #아몬드봉봉',
+        price: 19500
+      },
+      {
+        id: 'cake_06',
+        image: process.env.PUBLIC_URL+'/img/cake/cake06.png',
+        desc: '고소한 아몬드 타르트와 상큼달콤한 피치맛 요거트의 조화!',
+        title: '베이비 피치 타르트',
+        tag : '#베이비피치타르트 #피치요거트',
+        price: 19500
+      },
+    
+    
+    
+    
+    ]
+  
+    export default cakeData
