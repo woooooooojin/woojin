306c473df3b33ffccd9a7ffc2661e565d4b0e2b1
woojin <kevin2205@naver.com>
2023-06-27 16:53:27 +0900

    0627

--- React_project/react_site/src/pages/cakeData.js
+++ React_project/react_site/src/pages/cakeData.js
@@ -26,7 +26,7 @@ const cakeData = [
       {
         id: 'cake_04',
         image: process.env.PUBLIC_URL+'/img/cake/cake04.png',
-        desc: '부끄러운 기린의 귀여운 뒷모습을 표현한 케이크!',
+        desc: '고소한 아몬드 타르트와 사과맛 아이스크림의 조화!',
         title: '배이비 애플 타르트',
         tag : '#애플샤베트 #애플타르트',
         price: 19500
@@ -34,7 +34,7 @@ const cakeData = [
       {
         id: 'cake_05',
         image: process.env.PUBLIC_URL+'/img/cake/cake05.png',
-        desc: '달콤한 아이스크림을 즐기러 피크닉을 떤나 구름 카페속 시나모롤!',
+        desc: '달콤한 아이스크림을 즐기러 피크닉을 떠난 구름 카페속 시나모롤!',
         title: '시나모롤의 구름 속 피크닉',
         tag : '#베리베리스토로베리 #아몬드봉봉',
         price: 19500
