abfc979a98de0d288ddf96ffb076303989ee0c8b
woojin <kevin2205@naver.com>
2023-06-28 14:50:18 +0900

    0628

---
+++ React_project/react_fila/src/components/Sns.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import './components.css' 
+import styled from 'styled-components'
+
+
+const SnsBox = styled.div`
+
+    width: calc(20% - 24px);
+    /* height: 335px; */
+    height: auto;
+    cursor: pointer;
+    font-size: 16px;
+    background-color: transparent;
+    transition: .3s;
+    margin-left: 20px;
+    margin-bottom: 20px;
+    overflow: hidden;
+    &:nth-child(5n){
+        margin-right: 20px;
+    }
+ `
+
+
+
+export default function Sns() {
+
+    const snsData= [
+        {
+          id: 'sns01',
+          image: process.env.PUBLIC_URL+'/img/main/instagram/01.jpg',
+        },
+        {
+            id: 'sns02',
+            image: process.env.PUBLIC_URL+'/img/main/instagram/02.jpg',
+          },
+          {
+            id: 'sns03',
+            image: process.env.PUBLIC_URL+'/img/main/instagram/03.jpg',
+          },
+          {
+            id: 'sns04',
+            image: process.env.PUBLIC_URL+'/img/main/instagram/04.jpg',
+          },
+          {
+            id: 'sns05',
+            image: process.env.PUBLIC_URL+'/img/main/instagram/05.jpg',
+          },
+       
+          {
+            id: 'sns06',
+            image: process.env.PUBLIC_URL+'/img/main/instagram/06.jpg',
+          },
+          {
+            id: 'sns07',
+            image: process.env.PUBLIC_URL+'/img/main/instagram/07.jpg',
+          },
+          {
+            id: 'sns08',
+            image: process.env.PUBLIC_URL+'/img/main/instagram/08.jpg',
+          },
+          {
+            id: 'sns09',
+            image: process.env.PUBLIC_URL+'/img/main/instagram/09.jpg',
+          },
+          {
+            id: 'sns10',
+            image: process.env.PUBLIC_URL+'/img/main/instagram/10.jpg',
+          },
+          
+       
+       
+      ]
+
+
+  return (
+    <div>
+
+        <div className="sns_wrap">
+
+            {
+                snsData.map((val,idx)=>{
+                    return(
+
+                        <SnsBox>
+                            <img src={val.image} alt="img" />
+                        </SnsBox>
+
+
+                    )
+
+                })
+            }
+
+        </div>
+
+    </div>
+  )
+}
--- React_project/react_fila/src/pages/Home.jsx
+++ React_project/react_fila/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import VisualMain from '../components/VisualMain'
 import PopSwiper from '../components/PopSwiper'
 import './style.css' 
 import Magazine from '../components/Magazine'
+import Sns from '../components/Sns'
 
 
 export default function Home() {
@@ -13,13 +14,23 @@ export default function Home() {
 
     
       <div className="home_wrap">
-        <div className="pop_title">
+        <div className="home_title">
           <h4>지금 많이 찾는 상품</h4>
         </div>
 
         <PopSwiper />
 
+        <div className="home_title">
+          <h4>MAGAZINE</h4>
+        </div>
+
         <Magazine />
+
+        <div className="home_title">
+          <h4>SNS</h4>
+        </div>
+
+        <Sns/>
 
       </div>
 
