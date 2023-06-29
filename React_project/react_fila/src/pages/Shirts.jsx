dc522b6a641eb11375b6202714232c5c86e75ed7
woojin <kevin2205@naver.com>
2023-06-29 12:52:05 +0900

    0629

--- React_project/react_fila/src/pages/Shirts.jsx
+++ React_project/react_fila/src/pages/Shirts.jsx
@@ -2,18 +2,68 @@ import React from 'react'
 import './style.css' 
 import{Routes, Route, Link, useNavigate, useParams} from 'react-router-dom'; 
 import {useState} from 'react'
+import styled from 'styled-components'
 
 import { useDispatch } from 'react-redux';
 import { addItem } from '../pages/store';
+import shirtsData from './shirtsData';
+
+
+const Button = styled.button`
+
+    width: calc(50% - 30px);
+    margin-left: 20px;
+    height: 40px;
+    border: 1px solid #ccc;
+    border-radius: 30px;
+    margin-top: 20px;
+    cursor: pointer;
+    font-size: 16px;
+    background-color: transparent;
+    transition: .3s;
+    &:hover{
+        background-color: #002053;
+        color:#fff;
+    }
+    &:nth-child(2){
+        margin-right: 20px;
+    }
+ `
 
 
 export default function Shirts() {
+  const [shirts] = useState(shirtsData)
+  const dispatch = useDispatch()
 
-    
   return (
     <div>
 
-      <div className="item_wrap">
+      <div className="category_tit">
+        <h2>Shirts</h2>
+      </div>
+
+      <div className="itembox_wrap">
+
+        {
+          shirts.map((shirt,idx) => {
+            return(
+
+              <div className="item_box" key={idx}>
+                <Link>
+                  <div className="item_img_wrap">
+                    <img src={shirt.image} alt="img"/>
+                  </div>
+                  <h4 className='item_tit'>{shirt.title}</h4>
+                  <p className='item_tag'>{shirt.tag}</p>
+                  <p className='item_price'>{shirt.price}</p>
+                </Link>
+                <Button onClick={()=>{dispatch(addItem({id: shirt.id, img: shirt.image, title: shirt.title, price : shirt.price ,count: 1}), alert('장바구니에 담겼습니다.'))}}>장바구니</Button>
+                <Button onClick={()=>{dispatch(addItem({id: shirt.id, img: shirt.image, title: shirt.title, price : shirt.price ,count: 1}), alert('장바구니를 확인해주세요.'))}}>구매하기</Button>
+              </div>
+
+            )
+          })
+        }
 
 
 
--- React_project/react_fila/src/pages/shirtsData.js
+++ React_project/react_fila/src/pages/shirtsData.js
@@ -1,12 +1,84 @@
 const shirtsData= [
     {
       id: 'shirts_01',
-      image: process.env.PUBLIC_URL+'/img/rank/shirt_pants/01.jpg',
-      desc: '!',
-      title: '피치블라스트',
-      tag : '#복숭아맛 #블라스트',
-      price: 4800
+      image: process.env.PUBLIC_URL+'/img/main/shirts/shirts01.jpg',
+      desc: '가볍고 신축성이 좋으며 우수한 터치감과 통기성으로 땀배출이 좋은 잠재권축사 소재의 티셔츠입니다.',
+      title: '퍼포먼스 그래픽 반팔티',
+      tag : '#남성 #레귤러핏 #트레이닝',
+      price: 49000
     },
+    {
+        id: 'shirts_02',
+        image: process.env.PUBLIC_URL+'/img/main/shirts/shirts02.jpg',
+        desc: '가볍고 신축성이 좋으며 우수한 터치감과 통기성으로 땀배출이 좋은 잠재권축사 소재의 티셔츠입니다.',
+        title: '퍼포먼스 그래픽 반팔티',
+        tag : '#남성 #레귤러핏 #트레이닝',
+        price: 49000
+      },
+      {
+        id: 'shirts_03',
+        image: process.env.PUBLIC_URL+'/img/main/shirts/shirts03.jpg',
+        desc: '부드러운 터치감과 높은 퀄리티의 면/폴리에스터 이중지 소재입니다.',
+        title: 'FILA ON BEACH 반팔티',
+        tag : '#공용 #컴포트핏 #트레이닝',
+        price: 59000
+      },
+      {
+        id: 'shirts_04',
+        image: process.env.PUBLIC_URL+'/img/main/shirts/shirts04.jpg',
+        desc: '부드러운 터치감과 높은 퀄리티의 면/폴리에스터 이중지 소재입니다.',
+        title: '하트 로고 반팔티',
+        tag : '#공용 #컴포트핏 #트레이닝',
+        price: 49000
+      },
+      {
+        id: 'shirts_05',
+        image: process.env.PUBLIC_URL+'/img/main/shirts/shirts05.jpg',
+        desc: '부드러운 터치감과 높은 퀄리티의 면/폴리에스터 이중지 소재입니다.',
+        title: '하트로고 반팔티',
+        tag : '#공용 #컴포트핏 #트레이닝',
+        price: 49000
+      },
+      {
+        id: 'shirts_06',
+        image: process.env.PUBLIC_URL+'/img/main/shirts/shirts06.jpg',
+        desc: '부드러운 터치감과 높은 퀄리티의 면/폴리에스터 이중지 소재입니다.',
+        title: '아티스트 이진 반팔티',
+        tag : '#공용 #컴포트핏 #트레이닝',
+        price: 49000
+      },
+      {
+        id: 'shirts_07',
+        image: process.env.PUBLIC_URL+'/img/main/shirts/shirts07.jpg',
+        desc: '우수한 형태 안정성이 특징인 면/폴리에스터 이중지 소재를 사용했습니다.',
+        title: '아티스트 이진 크롭 반팔티',
+        tag : '#여성 #컴포트핏 #트레이닝',
+        price: 69000
+      },
+      {
+        id: 'shirts_08',
+        image: process.env.PUBLIC_URL+'/img/main/shirts/shirts08.jpg',
+        desc: '우수한 형태 안정성이 특징인 면/폴리에스터 이중지 소재를 사용했습니다.',
+        title: '아티스트 이진 크롭 반팔티',
+        tag : '#여성 #컴포트핏 #트레이닝',
+        price: 69000
+      },
+      {
+        id: 'shirts_09',
+        image: process.env.PUBLIC_URL+'/img/main/shirts/shirts09.jpg',
+        desc: '우수한 형태 안정성이 특징인 면/폴리에스터 이중지 소재를 사용했습니다.',
+        title: '아티스트 이진 크롭 반팔티',
+        tag : '#여성 #컴포트핏 #트레이닝',
+        price: 69000
+      },
+      {
+        id: 'shirts_10',
+        image: process.env.PUBLIC_URL+'/img/main/shirts/shirts10.jpg',
+        desc: '흡습속건 및 컬러 표현이 우수한 잠재권축사를 사용한 그래픽 반팔티입니다.',
+        title: '아티스트 이진 반팔티',
+        tag : '#공용 #컴포트핏 #트레이닝',
+        price: 49000
+      },
    
     
   ]
