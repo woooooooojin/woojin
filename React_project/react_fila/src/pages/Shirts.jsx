4f74f25f3dc6f939c366c9014311881bc5ffc70d
woojin <kevin2205@naver.com>
2023-06-29 12:11:29 +0900

    0629

--- React_project/react_fila/src/pages/Shirts.jsx
+++ React_project/react_fila/src/pages/Shirts.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import './style.css' 
+import{Routes, Route, Link, useNavigate, useParams} from 'react-router-dom'; 
+import {useState} from 'react'
+
+import { useDispatch } from 'react-redux';
+import { addItem } from '../pages/store';
+
 
 export default function Shirts() {
+
+    
   return (
-    <div>Shirts</div>
+    <div>
+
+      <div className="item_wrap">
+
+
+
+
+
+      </div>
+
+
+    </div>
   )
 }
---
+++ React_project/react_fila/src/pages/shirtsData.js
@@ -0,0 +1,14 @@
+const shirtsData= [
+    {
+      id: 'shirts_01',
+      image: process.env.PUBLIC_URL+'/img/rank/shirt_pants/01.jpg',
+      desc: '!',
+      title: '피치블라스트',
+      tag : '#복숭아맛 #블라스트',
+      price: 4800
+    },
+   
+    
+  ]
+
+export default shirtsData
