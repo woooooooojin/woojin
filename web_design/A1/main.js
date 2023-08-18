827a06da256ede74df490b9930d2adfeb115f277
woooooooojin <kevin2205@naver.com>
2023-08-18 14:01:03 +0900

    a1

--- web_design/A1/main.js
+++ web_design/A1/main.js
@@ -17,3 +17,37 @@ $(function(){
     }, 3000);
 
 })
+
+const tabNotice = document.querySelector('.notice')
+const tabGallary= document.querySelector('.gallary')
+
+const noticeList = document.querySelector('.notice_list')
+const gallaryList = document.querySelector('.gallary_list')
+
+tabNotice.addEventListener('click',function(){
+    tabNotice.classList.add('on')
+    tabGallary.classList.remove('on')
+    noticeList.style.display='block'
+    gallaryList.style.display='none'
+    
+})
+tabGallary.addEventListener('click',()=>{
+    tabGallary.classList.add('on')
+    tabNotice.classList.remove('on')
+    gallaryList.style.display='block'
+    noticeList.style.display = 'none'
+
+   
+})
+
+
+const firstNotice = document.querySelector('.first_notice')
+const popup = document.querySelector('.pop')
+const closeBtn = document.querySelector('.close')
+
+firstNotice.addEventListener('click',function(){
+    popup.style.display = 'block'
+})
+closeBtn.addEventListener('click',function(){
+    popup.style.display='none'
+})
