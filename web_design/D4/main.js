77fe84766df9a013b4c6bca3fea3cb72bd30816b
woooooooojin <kevin2205@naver.com>
2023-08-24 15:54:17 +0900

    d4

--- web_design/D4/main.js
+++ web_design/D4/main.js
@@ -14,4 +14,18 @@ $(function(){
 
     }, 2500);
 
+    // setInterval(() => {
+    //     $('.slide_wrap').animate({'margin-top':'-400px'},function(){
+    //         $('.slide:first').appendTo('.slide_wrap')
+    //         $('.slide_wrap').css({'margin-top':'0'})
+    //     })
+    // }, 2500);
+
+    // setInterval(() => {
+    //     $('.slide_wrap').animate({'margin-left':'-100%'},function(){
+    //         $('.slide:first').appendTo('.slide_wrap')
+    //         $('.slide_wrap').css({'margin-left':'0%'})
+    //     })
+    // }, 2500);
+
 })
